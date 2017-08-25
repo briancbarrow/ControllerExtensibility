0722be1b27c5b690ca1c79ffa287b68369f0e7c3
Brian Barrow <briancbarrow@gmail.com>
2017-08-25 15:43:09 -0600

    custom action invoker

---
+++ ControllerExtensibility/Infrastructure/CustomActionInvoker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace ControllerExtensibility.Infrastructure
+{
+    public class CustomActionInvoker : IActionInvoker 
+    {
+        public bool InvokeAction(ControllerContext controllerContext, string actionName)
+        {
+            if(actionName == "Index")
+            {
+                controllerContext.HttpContext.Response.Write("this is output from the Index action");
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
