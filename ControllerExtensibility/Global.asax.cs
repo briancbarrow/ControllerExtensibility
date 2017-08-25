dd164f48e373f9d8e3c594b6d6dea8f0fbf20f44
Brian Barrow <briancbarrow@gmail.com>
2017-08-25 15:32:09 -0600

    Intercepting instantiation requests using a custom controller activator

--- ControllerExtensibility/Global.asax.cs
+++ ControllerExtensibility/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
 
 namespace ControllerExtensibility
 {
@@ -13,6 +14,9 @@ namespace ControllerExtensibility
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new
+                DefaultControllerFactory(new CustomControllerActivator()));
         }
     }
 }
